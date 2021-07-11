0f0bf2c3ae56b0778aaddfa88728912d4fc4302f
Christophe Hurpeau <christophe@hurpeau.com>
2021-07-11 18:54:41 +0200

    chore: scripts/check-package.mjs

--- scripts/check-package.mjs
+++ scripts/check-package.mjs
@@ -1,6 +1,5 @@
-'use strict';
-
-const { createCheckPackage } = require('..');
+// eslint-disable-next-line import/extensions
+import { createCheckPackage } from '../dist/index-node12.cjs.js';
 
 createCheckPackage().checkRecommended({
   isLibrary: true,
