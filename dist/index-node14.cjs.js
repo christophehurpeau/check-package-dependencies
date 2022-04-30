baeb480ffbc3333da5882bbbfa43026325c89c81
Christophe Hurpeau <christophe@hurpeau.com>
2022-04-30 23:54:32 +0200

    chore: update pob/root and build

--- dist/index-node14.cjs.js
+++ dist/index-node14.cjs.js
@@ -126,19 +126,19 @@ function checkPeerDependencies(pkg, reportError, type, allowedPeerIn, depPkg, mi
   if (!peerDependencies) return;
   const allowedPeerInExisting = allowedPeerIn.filter(allowedPeerInType => pkg[allowedPeerInType]);
 
-  for (const [peerDepKey, range] of Object.entries(peerDependencies)) {
-    const versionsIn = allowedPeerInExisting.filter(allowedPeerInExistingType => pkg[allowedPeerInExistingType][peerDepKey]);
+  for (const [peerDepName, range] of Object.entries(peerDependencies)) {
+    const versionsIn = allowedPeerInExisting.filter(allowedPeerInExistingType => pkg[allowedPeerInExistingType]?.[peerDepName]);
 
     if (versionsIn.length === 0) {
-      const peerDependenciesMetaPeerDep = peerDependenciesMeta?.[peerDepKey];
+      const peerDependenciesMetaPeerDep = peerDependenciesMeta?.[peerDepName];
 
       if (peerDependenciesMetaPeerDep?.optional) {
         return;
       }
 
-      reportError(`Missing "${peerDepKey}" peer dependency from "${depPkg.name}" in ${type}`, `it should satisfies "${range}" and be in ${allowedPeerIn.join(' or ')}`, missingOnlyWarnsForCheck.shouldWarnsFor(peerDepKey));
+      reportError(`Missing "${peerDepName}" peer dependency from "${depPkg.name}" in ${type}`, `it should satisfies "${range}" and be in ${allowedPeerIn.join(' or ')}`, missingOnlyWarnsForCheck.shouldWarnsFor(peerDepName));
     } else {
-      const versions = versionsIn.map(versionsInType => pkg[versionsInType][peerDepKey]);
+      const versions = versionsIn.map(versionsInType => pkg[versionsInType][peerDepName]);
       versions.forEach((version, index) => {
         if (version.startsWith('npm:')) {
           return;
@@ -149,7 +149,7 @@ function checkPeerDependencies(pkg, reportError, type, allowedPeerIn, depPkg, mi
         if (!minVersionOfVersion || !semver__default.satisfies(minVersionOfVersion, range, {
           includePrerelease: true
         })) {
-          reportError(`Invalid "${peerDepKey}" peer dependency`, `"${version}" (in ${allowedPeerInExisting[index]}) should satisfies "${range}" from "${depPkg.name}" ${type}`, invalidOnlyWarnsForCheck.shouldWarnsFor(peerDepKey));
+          reportError(`Invalid "${peerDepName}" peer dependency`, `"${version}" (in ${allowedPeerInExisting[index]}) should satisfies "${range}" from "${depPkg.name}" ${type}`, invalidOnlyWarnsForCheck.shouldWarnsFor(peerDepName));
         }
       });
     }
