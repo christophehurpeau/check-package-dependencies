c59561b60f5d754b0f848a5fc3d3834acdcce06c
Christophe Hurpeau <christophe@hurpeau.com>
2023-01-29 17:02:24 +0100

    build: build dist

--- dist/definitions/utils/__mocks__/createReportError.d.ts
+++ dist/definitions/utils/__mocks__/createReportError.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="jest" />
-export declare const mockReportError: jest.Mock<any, any>;
-export declare const createReportError: jest.Mock<any, any>;
+export declare const mockReportError: jest.Mock<any, any, any>;
+export declare const createReportError: jest.Mock<any, any, any>;
 //# sourceMappingURL=createReportError.d.ts.map
