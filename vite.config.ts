5df809cb41e396bf8ccf14639b24ce2ff43cf70f
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-04-26 00:12:19 +0200

    chore: fix coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
+console.log(process.env.POB_VITEST_COVERAGE);
+
 export default defineConfig({
   test: {
     setupFiles: ['src/test-setup.ts'],
     include: ['src/**/__tests__/**/*.ts?(x)', 'src/**/*.test.ts?(x)'],
     coverage: {
+      provider: 'v8',
       include: ['src/**/*.ts?(x)'],
       reportsDirectory: 'docs/coverage',
       reporter: (process.env.POB_VITEST_COVERAGE || 'json,text').split(','),
